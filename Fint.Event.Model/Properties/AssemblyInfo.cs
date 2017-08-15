cc8f6e5d5ec5b5c9c01e75b1aa72d2933d1a4362
Peter Samuel <pjsamuel3@hotmail.com>
2017-08-15 16:05:36 +0200

    Updated csproj with nuget version number. Upgraded to .netstandard2.0

--- Fint.Event.Model/Properties/AssemblyInfo.cs
+++ Fint.Event.Model/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FintEventModel")]
+[assembly: AssemblyTitle("Fint.Event.Model")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FintEventModel")]
+[assembly: AssemblyProduct("Fint.Event.Model")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
