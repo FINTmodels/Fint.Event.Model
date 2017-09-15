06e721dcc38aedbf57c9851431306f0b26db55d9
Hakan Silfvernagel <hakan.silfvernagel@miles.no>
2017-09-15 13:31:59 +0200

    Updated Status

--- Fint.Event.Model/Model/Status.cs
+++ Fint.Event.Model/Model/Status.cs
@@ -5,18 +5,20 @@
     /// <ul>
     ///  <li>{@link #NEW}</li>
     ///  <li>{@link #DOWNSTREAM_QUEUE}</li>
-    ///  <li>{@link #DELIVERED_TO_PROVIDER}</li>
-    ///  <li>{@link #PROVIDER_ACCEPTED}</li>
-    ///  <li>{@link #PROVIDER_NOT_CONFIRMED}</li>
+    ///  <li>{@link #DELIVERED_TO_ADAPTER}</li> 
+    ///  <li>{@link #ADAPTER_ACCEPTED}</li> 
+    ///  <li>{@link #ADAPTER_NOT_CONFIRMED}</li> 
     ///  <li>{@link #UNABLE_TO_DELIVER}</li>
-    ///  <li>{@link #NO_RESPONSE_FOR_PROVIDER}</li>
-    ///  <li>{@link #PROVIDER_REJECTED}</li>
-    ///  <li>{@link #PROVIDER_RESPONSE}</li>
+    ///  <li>{@link #NO_RESPONSE_FROM_ADAPTER}</li> 
+    ///  <li>{@link #ADAPTER_REJECTED}</li> 
+    ///  <li>{@link #ADAPTER_RESPONSE}</li> 
     ///  <li>{@link #UPSTREAM_QUEUE}</li>
     ///  <li>{@link #TEMP_UPSTREAM_QUEUE}</li>
     ///  <li>{@link #SENT_TO_CLIENT}</li>
     ///  <li>{@link #CACHE}</li>
     ///  <li>{@link #CACHE_RESPONSE}</li>
+    ///  <li>{@link #ERROR}</li> 
+    ///  <li>{@link #ADAPTER_RESPONSE_ORPHANED}</li> 
     /// </ul>
     /// </summary>
     public enum Status
@@ -32,22 +34,22 @@
         DOWNSTREAM_QUEUE = 11,
 
         /// <summary>
-        /// Indicates that the event is delivered to the provider, but we don't know if
+        /// Indicates that the event is delivered to the adapter, but we don't know if
         /// the provider will process the event yet.
         /// </summary>
-        DELIVERED_TO_PROVIDER = 12,
+        DELIVERED_TO_ADAPTER = 12,
 
         /// <summary>
         /// Indicates that the event is accepted and understood by the adapter. The adapter will
         /// process the event.
         /// </summary>
-        PROVIDER_ACCEPTED = 13,
+        ADAPTER_ACCEPTED = 13,
 
         /// <summary>
-        /// Indicates that we never to a confirmation from the provider after delivering it according
+        /// Indicates that we never received a confirmation from the adapter after delivering it according
         /// to the TTL value.
         /// </summary>
-        PROVIDER_NOT_CONFIRMED = 103,
+        ADAPTER_NOT_CONFIRMED = 103,
 
         /// <summary>
         /// Indicates that the event could not be delivered to the provider.
@@ -55,22 +57,22 @@
         UNABLE_TO_DELIVER = 104,
 
         /// <summary>
-        /// Indicates that the event is delivered to the provider, but the provider never responded
+        /// Indicates that the event is delivered to the adapter, but the adapter never responded
         /// according to the TTL value.
         /// </summary>
-        NO_RESPONSE_FOR_PROVIDER = 101,
+        NO_RESPONSE_FROM_ADAPTER = 101,
 
         /// <summary>
-        /// Indicates that the provider recieved the event, but could not process it. Reasons could be:
+        /// Indicates that the adapter recieved the event, but could not process it. Reasons could be:
         /// - could not understand the event
         /// - could not communicate with the source system
         /// </summary>
-        PROVIDER_REJECTED = 102,
+        ADAPTER_REJECTED = 102,
 
         /// <summary>
-        /// Indicates that the event is the response from the provider.
+        /// Indicates that the event is the response from the adapter.
         /// </summary>
-        PROVIDER_RESPONSE = 14,
+        ADAPTER_RESPONSE = 14,
 
         /// <summary>
         /// Indicates that the event is put in the upstream event queue.
@@ -91,19 +93,22 @@
         /// Indicates that the event is delivered to the cache service.
         /// </summary>
         CACHE = 50,
+
         /// <summary>
         /// Indicates that the event is the response form the cache service.
         /// </summary>
         CACHE_RESPONSE = 51,
+
         /// <summary>
         /// Indicates that something went wrong with the event. The message field should be set when this
         /// status is set.
         /// </summary>
         ERROR = 1000,
+
         /// <summary>
         /// Indicates that we cannot find any corresponding events.
         /// </summary>
-        PROVIDER_RESPONSE_ORPHANT = 100
+        ADAPTER_RESPONSE_ORPHANED = 100
 
     }
 }
